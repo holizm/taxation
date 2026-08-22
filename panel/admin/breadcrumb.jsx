7483b66abe5593ae971acb3f7017b8e9d8087d57
Saeed Nemati <sn.1361@gmail.com>
2026-08-22 15:00:56 +0330

    committed by script

---
+++ panel/admin/breadcrumb.jsx
@@ -0,0 +1,8 @@
+import PartBreadcrumb from 'partBreadcrumb'
+
+export default props => <PartBreadcrumb
+    icon='percent'
+    name='taxation'
+    title='taxationTaxJurisdictions'
+    {...props}
+/>
