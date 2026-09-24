67b1339652f9b541d14a125f6c85f05fbd515e47
Saeed Nemati <sn.1361@gmail.com>
2026-09-24 09:08:48 +0330

    committed by script

--- panel/admin/taxJurisdiction/list.jsx
+++ panel/admin/taxJurisdiction/list.jsx
@@ -1,14 +1,8 @@
 import { List } from 'list'
-import filters from './filters'
 import form from './form'
-import headers from './headers'
-import row from './row'
 
 export default <List
     create={form}
-    filters={filters}
     hasDelete
     hasEdit
-    headers={headers}
-    row={row}
 />
